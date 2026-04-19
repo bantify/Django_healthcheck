9788e70933e3ac6266e5058c82a4cc1b255ad63e
nbanik <nbanik@gmail.com>
2026-04-19 12:26:37 +0600

    commit all from pycharm

--- healthcheck/urls.py
+++ healthcheck/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from healthcheck.views import GenerateHealthCheckReportView
 
 urlpatterns = [
-    path('', GenerateHealthCheckReportView.as_view(), name='hello_world'),
+    path('', GenerateHealthCheckReportView.as_view(), name='home'),
 ]
